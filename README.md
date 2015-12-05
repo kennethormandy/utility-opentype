134eed9f89e04add5b1e6a7e13189d8122175d80
Kenneth Ormandy <kenneth@chloi.io>
2015-12-05 13:27:46 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Utility OpenType
+
+Immutable advanced typographic feature utilities that degrade gracefully with CSS alone. Works great on its own or with [Basscss](http://basscss.com).
+
+## License
+
+[The MIT License (MIT)](LICENSE.md)
+
+Copyright © 2015 [Kenneth Ormandy](http://kennethormandy.com)
---
+++ test/test.js
@@ -0,0 +1,64 @@
+// MIT via http://git.io/vR4SG
+
+var fs = require('fs')
+var path = require('path')
+var assert = require('assert')
+var cssnext = require('cssnext')
+var cssstats = require('cssstats')
+var mixed = require('css-mixed-properties')
+var Cleancss = require('clean-css')
+
+var src = fs.readFileSync(path.join(__dirname, '../index.css'), 'utf8')
+var css
+var stats
+
+describe('utility-opentype', function () {
+
+  it('should compile', function () {
+    assert.doesNotThrow(function () {
+      css = cssnext(src)
+    })
+  })
+
+  it('should compile to string', function () {
+    assert.equal(typeof css, 'string')
+  })
+
+  it('should be css', function () {
+    assert.doesNotThrow(function () {
+      stats = cssstats(css, { safe: false })
+    })
+  })
+
+  it('should have rules', function () {
+    assert(stats.rules.total > 0)
+  })
+
+  it('should have declarations', function () {
+    assert(stats.declarations.total > 0)
+  })
+
+  it('should have properties', function () {
+    assert(Object.keys(stats.declarations.properties).length > 0)
+  })
+
+  it('should not have a high mix of structure and skin properties', function () {
+    var mix = mixed(css)
+    var max = 0
+    mix.warnings.forEach(function (warning) {
+      if (warning.score > max) {
+        max = warning.score
+      }
+    })
+    assert(max < 5)
+  })
+
+  it('should not break perf budget', function () {
+    var max = 1750
+    var minified = new Cleancss({ advanced: false, }).minify(css).styles
+    var minifiedStats = cssstats(minified, { safe: false })
+
+    assert(minifiedStats.gzipSize <= max)
+  })
+
+})
